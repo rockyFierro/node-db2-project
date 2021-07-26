8793d3251fc6d0a8b7270c331b803f9de5fc05b1
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-07-26 00:56:15 -0700

    initialized router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,13 @@
-// DO YOUR MAGIC
+const express = require('express');
+const router = express.Router();
+const Cars = require('./cars-model');
+
+router.route('/api/cars')
+  .get(
+    (req,res,next) => {
+      return Cars.getAll();
+    }
+  );
+
+  module.exports = router;
+  
