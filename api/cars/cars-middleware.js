d41fc4059440a6e1f0c499e6afc61e4b04d410cb
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-07-26 19:52:34 -0700

    check Vin number

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,57 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const database = require('../../data/db-config.js');
+const Cars = require('./cars-model');
+const vinCheck = require('vin-validator');
+
+exports.checkCarId = async (req, res, next) => {
+  try {
+    const carID = await Cars.getById(req.params.id);
+    if (!carID) {
+      next({
+        status: 400,
+        message: `vim ${req.params.id} is invalid`
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
-const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkCarPayload = (req, res, next) => {
+  const err = { status: 400 };
+  const {
+    vin, //required string
+    make, //unique string
+    model, // required string
+    mileage, //required int
+    title, //required string
+  } = req.body;
+
+  const required = { vin, make, model, mileage, title };
+
+  for (const field in required) {
+    if (field === undefined) {
+      err.message = `${field} is missing`;
+      next(err);
+    } else {
+      next();
+    }
+  }
+};
+
+exports.checkVinNumberValid = (req, res, next) => {
+  const { vin } = req.body;
+  const realVin = vinCheck.validate(vin);
+
+  if (realVin === false) {
+    next({
+      status: 400,
+      message: `Vin ${vin} is invalid`,
+    });
+  } else {
+    next();
+  }
 }
 
-const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
-}
-
-const checkVinNumberUnique = (req, res, next) => {
+exports.checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
 }
